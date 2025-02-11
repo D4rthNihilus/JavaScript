72bb8b7f881e6b2d0479f2860c739a3805a56b33
D4rthNihilus <rafaelguedes05013@gmail.com>
2025-02-11 12:18:47 -0300

    criei os exercícios 008, 009 e 010

---
+++ Exercicios/ex008/exercicio.js
@@ -0,0 +1,6 @@
+vel = 60.0
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+
+if (vel > 60) {
+    console.log ('Você ultrapassou a velocidade permitida. MULTADO!!!')
+}
---
+++ Exercicios/ex009/exercicio.js
@@ -0,0 +1,9 @@
+country = "France"
+console.log(`Living in ${country}`)
+
+if (country ==  "Brazil") {
+    console.log("You are Brazilian")
+}
+else {
+    console.log("You are Foreign")
+}
