e6ad5d5f7bf3a8e475971a3ab9d082356cac2bb0
D4rthNihilus <rafaelguedes05013@gmail.com>
2025-04-17 21:04:45 -0300

    Criei os exercícios ex016, ex017 e ex018

---
+++ Exercicios/ex016/ambiente.js
@@ -0,0 +1,23 @@
+var c = 0
+
+do {
+    console.log(`Passo ${c+1}`)
+    c++
+} while (c < 6)
+
+    
+
+while (c < 6) {
+    console.log(`Passo ${c+1}`)
+    c++
+} 
+
+
+/* 
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
---
+++ Exercicios/ex17/ambiente.js
@@ -0,0 +1,15 @@
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+
+var d = 2
+do {
+    console.log(d)
+    d++
+}while (d <= 5)
+
+for (var e = 1; e <= 5; e++){
+    console.log(e)
+}
