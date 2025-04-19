72a2a4ff2163458454e00b82b8d90788b746f75c
D4rthNihilus <rafaelguedes05013@gmail.com>
2025-04-18 21:30:14 -0300

    criei os exercícios ex019, ex020 e ex021

---
+++ Exercicios/ex020/arrays.js
@@ -0,0 +1,7 @@
+var num = [3, 6, 11, 4, 23, 7, 17, 13]
+
+var l = num.length
+
+for (var c = 0; c < l; c++) {
+    console.log(num[c])
+}
---
+++ Exercicios/ex021/function2.js
@@ -0,0 +1,5 @@
+function soma (m=0, n=0) {
+    return m + n
+}
+
+console.log(soma(5, 100))
